e746e30da32223f33c81ecefd2fe298ed281e9a1
nkws <keigo_nakagawa@voyagegroup.com>
2017-11-05 22:48:54 +0900

    create project

---
+++ summary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  summary
+//
+//  Created by 中川 慶悟 on 2017/11/05.
+//  Copyright © 2017年 ArgentVGL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
