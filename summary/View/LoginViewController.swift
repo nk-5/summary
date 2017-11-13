7676dd2cc197936746e84e16a36f890b2927ecfd
nkws <keigo_nakagawa@voyagegroup.com>
2017-11-13 23:24:53 +0900

    add MVVM directories, login viewController

---
+++ summary/View/LoginViewController.swift
@@ -0,0 +1,22 @@
+//
+//  LoginViewController.swift
+//  summary
+//
+//  Copyright © 2017年 ArgentVGL. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func didTouchLogin(_: Any) {
+    }
+}
