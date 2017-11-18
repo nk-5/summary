d0892da708b17a69a85e2929e2be4105843ed50d
nkws <keigo_nakagawa@voyagegroup.com>
2017-11-18 17:54:45 +0900

    create unit test directories

---
+++ summaryTests/ViewModels/LoginViewModelTest.swift
@@ -0,0 +1,24 @@
+//
+//  LoginViewModelTest.swift
+//  summaryTests
+//
+//  Copyright © 2017年 ArgentVGL. All rights reserved.
+//
+
+import XCTest
+
+class LoginViewModelTest: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testErrorDescription() {
+        let loginVM: LoginViewModel = LoginViewModel()
+    }
+
+}
