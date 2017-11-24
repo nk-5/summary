b36939d2ff63dea5c8171780e343d0e621e9f60a
nkws <keigo_nakagawa@voyagegroup.com>
2017-11-25 00:34:32 +0900

    add create rank form action button

--- summary/Views/RankFormViewController.swift
+++ summary/Views/RankFormViewController.swift
@@ -13,7 +13,8 @@ class RankFormViewController: FormViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // TODO: cannot text input
+        setFormActionButton()
+
         let rankTitle: String = "ランキング名"
         let rankTarget: String = "ランキング項目"
         let rankDescription: String = "ランキングの説明（任意）"
@@ -52,5 +53,38 @@ class RankFormViewController: FormViewController {
             <<< TextAreaRow { row in
                 row.placeholder = rankDescription
             }
+    }
+
+    func setFormActionButton() {
+        let actionStack: UIStackView = UIStackView(frame: CGRect(x: 0, y: view.frame.height - 200, width: view.frame.width, height: 100))
+        actionStack.axis = .horizontal
+        actionStack.alignment = .center
+        actionStack.distribution = .fillEqually
+
+        let cancel: UIButton = createButton(title: "キャンセル", selector: #selector(didTouchCancel))
+        let create: UIButton = createButton(title: "ランキング作成", selector: #selector(didTouchCreateRank))
+
+        actionStack.addArrangedSubview(cancel)
+        actionStack.addArrangedSubview(create)
+
+        tableView.addSubview(actionStack)
+    }
+
+    func createButton(title: String, selector: Selector) -> UIButton {
+        let button: UIButton = UIButton(frame: CGRect(x: 0, y: 0, width: 100, height: 50))
+        button.setTitle(title, for: .normal)
+        button.setTitleColor(view.tintColor, for: .normal)
+        button.addTarget(self, action: selector, for: .touchUpInside)
+        return button
+    }
+
+    @objc func didTouchCancel() {
+        // TODO: back home or room
+        print("cancel")
+    }
+
+    @objc func didTouchCreateRank() {
+        // TODO: create rank
+        print("create")
     }
 }
