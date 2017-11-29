a916cae5b1cccf652d1104ecc55a2841f1975a34
nkws <keigo_nakagawa@voyagegroup.com>
2017-11-30 01:09:54 +0900

    implement room rank list

--- summary/Views/RoomViewController.swift
+++ summary/Views/RoomViewController.swift
@@ -6,6 +6,43 @@
 //
 
 import UIKit
+import RxSwift
+import RxDataSources
+import RxCocoa
+
+enum RankState: Int {
+    case ready
+    case voting
+    case finished
+
+    func description() -> String {
+        switch self {
+        case .ready:
+            return "準備中"
+        case .voting:
+            return "開催中"
+        case .finished:
+            return "終了"
+        }
+    }
+}
+
+struct RoomRank {
+    var name: String
+    var state: String
+}
+
+struct SectionOfRoomRank {
+    var items: [Item]
+}
+
+extension SectionOfRoomRank: SectionModelType {
+    typealias Item = RoomRank
+
+    init(original _: SectionOfRoomRank, items: [Item]) {
+        self.items = items
+    }
+}
 
 class RoomViewController: UIViewController {
 
@@ -13,6 +50,26 @@ class RoomViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        let dataSource = RxTableViewSectionedReloadDataSource<SectionOfRoomRank>(
+            configureCell: { (_: TableViewSectionedDataSource<SectionOfRoomRank>, tv: UITableView, _: IndexPath, item: RoomRank) -> UITableViewCell in
+                let cell = tv.dequeueReusableCell(withIdentifier: "cell") ?? UITableViewCell(style: .default, reuseIdentifier: "cell")
+                cell.textLabel?.text = "\(item.name) 状態：\(item.state)"
+                return cell
+        })
+
+        // TODO: datasource get from Firestore
+        let section = [
+            SectionOfRoomRank(items: [
+                RoomRank(name: "test rank name", state: RankState.ready.description()),
+                RoomRank(name: "hogehoge rank", state: RankState.voting.description()),
+                RoomRank(name: "fugafuga rank", state: RankState.finished.description()),
+            ]),
+        ]
+
+        Observable.just(section)
+            .bind(to: rankView.rx.items(dataSource: dataSource))
+            .disposed(by: disposeBag)
     }
 
     @IBAction func didTouchCreateRank(_: Any) {
@@ -20,7 +77,7 @@ class RoomViewController: UIViewController {
         present(vc, animated: true, completion: nil)
     }
 
-    @IBAction func didTouchShare(_ sender: Any) {
+    @IBAction func didTouchShare(_: Any) {
         // TODO: modal display share options
         print("share")
     }
