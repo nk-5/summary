b7c517715a4aa7d3c1f58d4c76359139316c771b
nkws <keigo_nakagawa@voyagegroup.com>
2018-04-10 22:44:06 +0900

    add rank target view

--- summary/Views/VoteViewController.swift
+++ summary/Views/VoteViewController.swift
@@ -8,19 +8,37 @@
 import UIKit
 
 class VoteViewController: UIViewController {
-    @IBOutlet var rankName: UILabel!
+    @IBOutlet var rankTitle: UILabel!
+    @IBOutlet var targets: UIStackView!
 
     var rank: Rank?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // TODO: add error handling
-        // TODO: stackview-v add stackview-h
+        // TODO: set rank target
+
         guard let name = rank?.name else { return }
-        rankName.text = name
+        rankTitle.text = name
+
+        let target: UIStackView = UIStackView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 30))
+        target.axis = .horizontal
+        target.distribution = .fill
+
+        let testLabel = UILabel()
+        testLabel.text = "ranktarget"
+        target.addArrangedSubview(testLabel)
+
+        let testButton = UIButton()
+        testButton.setTitle("select", for: .normal)
+        testButton.setTitleColor(view.tintColor, for: .normal)
+        target.addArrangedSubview(testButton)
+
+        targets.insertArrangedSubview(target, at: 0)
     }
 
     @IBAction func didTouchCancel(_: Any) {
         dismiss(animated: true, completion: nil)
     }
+
 }
