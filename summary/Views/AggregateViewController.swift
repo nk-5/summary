f9ca46aff87453683ac90f7d0ace084ddc2eae2e
nkws <keigo_nakagawa@voyagegroup.com>
2018-05-11 00:06:56 +0900

    create AggregateVC

---
+++ summary/Views/AggregateViewController.swift
@@ -0,0 +1,16 @@
+//
+//  AggregateViewController.swift
+//  summary
+//
+//  Copyright © 2018年 ArgentVGL. All rights reserved.
+//
+
+import UIKit
+
+class AggregateViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
